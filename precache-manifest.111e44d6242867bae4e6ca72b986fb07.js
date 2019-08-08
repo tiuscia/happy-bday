8d5621290f65a325e78d90f522b612c1605ba2f7
katiazanotti <tiuscia.work@gmail.com>
2019-08-08 23:39:01 +0200

    Updates

---
+++ precache-manifest.111e44d6242867bae4e6ca72b986fb07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "824c50f2c4035e1dd9536d26a1941651",
+    "url": "/happy-bday/index.html"
+  },
+  {
+    "revision": "abbf5876b842c1c7d17d",
+    "url": "/happy-bday/static/css/main.3b055360.chunk.css"
+  },
+  {
+    "revision": "6a372a1457123db0d6ae",
+    "url": "/happy-bday/static/js/2.674b1d02.chunk.js"
+  },
+  {
+    "revision": "abbf5876b842c1c7d17d",
+    "url": "/happy-bday/static/js/main.18e49426.chunk.js"
+  },
+  {
+    "revision": "5a15c6dbcadc77e13c98",
+    "url": "/happy-bday/static/js/runtime~main.d2891497.js"
+  },
+  {
+    "revision": "7dc7a23a5d1d0dc3675b0558e43713ba",
+    "url": "/happy-bday/static/media/arrow-right.7dc7a23a.svg"
+  },
+  {
+    "revision": "2de0ada3759cbd8cf7d139bc79d3da80",
+    "url": "/happy-bday/static/media/cheers.2de0ada3.svg"
+  }
+]);
